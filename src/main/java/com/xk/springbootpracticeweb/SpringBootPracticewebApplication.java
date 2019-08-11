9cb0c0cdf214347cecfa51a00e4384f603591cde
hl123 <1956286912@qq.com>
2019-08-11 15:02:28 +0800

    提交版本

---
+++ src/main/java/com/xk/springbootpracticeweb/SpringBootPracticewebApplication.java
@@ -0,0 +1,13 @@
+package com.xk.springbootpracticeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracticewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracticewebApplication.class, args);
+	}
+
+}
